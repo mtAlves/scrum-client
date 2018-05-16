a29717ea900b28fb9582802cbe5db1adf99c701b
Matheus Alves <mtzin44@gmail.com>
2018-05-16 10:09:54 -0300

    fix idFromUrl

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,7 +1,7 @@
 
 export const idFromUrl = (url) => {
   if (url) {
-    return url.endsWith('/') ? url.split('/').reverse()[1] : url.split('/').reverse()[0]
+    return url.endsWith('/') ? parseInt(url.split('/').reverse()[1]) : parseInt(url.split('/').reverse()[0])
   }
   return false
 }
